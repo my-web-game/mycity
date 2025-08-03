55f2125dec0559092d8d886992b4bb75c0ce9c55
ZhuElly <34325707+Elly2018@users.noreply.github.com>
2025-08-04 01:48:30 +0800

    Upload prefix base vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,7 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: "/mycity/",
   plugins: [tailwindcss()],
   server: {
     port: 3000,
